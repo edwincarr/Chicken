5e27a2ced664fc798594f840b660655512b38f93
edwincarr <edwincarrillo9@outlook.com>
2023-03-11 23:19:18 -0500

    Init: CNA, Dependencies, Firebase

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Chicken Shop</title>
+        <meta name="description" content="Chicken Pickup App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hello World!</h1>
+        <h1>test 123</h1>
+      </main>
+    </>
+  )
+}
