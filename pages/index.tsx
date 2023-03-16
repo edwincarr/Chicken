fcd6967de04ced12afb17a761270b71badcb207d
edwincarr <edwincarrillo9@outlook.com>
2023-03-16 16:24:34 -0400

    Design for index continued

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,33 @@ import Navbar from "@/components/navbar"
 import wing from '../public/chicken.png'
 import wing2 from '../public/chicken-2.png'
 import Image from "next/image";
-import { useState } from "react";
+import { Josefin_Sans } from 'next/font/google'
+
+const jose = Josefin_Sans({
+  weight: ['700'],
+  subsets:['latin']
+})
 
 export default function Home() {
-  const [loaded, setLoaded] = useState(false)
-  loaded
   return (
-    <div onLoad={() => setLoaded(true)}>
+    <div>
       <Navbar />
-      <div className="bg-blob bg-cover min-h-screen aspect-16/9 pt-16" >
-        <div className="flex md:justify-center self-center max-md:flex-col gap-96">
-          <div className="max-md:pl-20 text-8xl w-72">Winner Winner Chicken Dinner</div>
+      <div className="bg-blob bg-cover min-h-screen aspect-16/9 flex-col pt-36 overflow-visible" >
+        <div className="flex flex-row-reverse justify-end self-center max-md:flex-col lg:justify-center lg:gap-20">
+          <div className={`max-md:pl-4 text-8xl w-72 ${jose.className}`}>Winner Winner Chicken Dinner</div>
           <div className="w-80 ">
-            <Image src={wing} alt='wing' className=""/>
+            <Image src={wing} alt='wing' className="-translate-y-10"/>
+          </div>
+        </div>
+
+        <div className="flex justify-start self-center max-md:flex-col lg:justify-center lg:gap-20 mb-20">
+          <div className={`max-md:pl-4 text-8xl w-96 ${jose.className}`}>Come Try Our Award Winning Chicken</div>
+          <div className="w-80">
+            <Image src={wing2} alt='wing' className="translate-y-20"/>
           </div>
         </div>
       </div>
+        <div className="h-screen">hi</div>
     </div>
   )
 }
