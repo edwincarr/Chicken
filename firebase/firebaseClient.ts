99560119bb3aa9d6560df185c1b923ce82c4885f
edwincarr <edwincarrillo9@outlook.com>
2023-03-24 07:23:39 -0400

    Env

--- firebase/firebaseClient.ts
+++ firebase/firebaseClient.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 import "firebase/firestore";
 import { collection, getFirestore } from 'firebase/firestore';
+import { env } from 'process';
 
 const clientCredentials = {
-  apiKey: "AIzaSyD9xKGViSpeBufoIwCEaVWIYXKBbKQeFWQ",
-  authDomain: "chicken-93a5c.firebaseapp.com",
-  projectId: "chicken-93a5c",
-  storageBucket: "chicken-93a5c.appspot.com",
-  messagingSenderId: "33405586872",
-  appId: "1:33405586872:web:f935e9021e9112ab6c134d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
